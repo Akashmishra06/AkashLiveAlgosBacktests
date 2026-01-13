91727768887ee9e4016216b8207545960b2303f1
Akash Mishra <akashmishra.py@gmail.com>
2026-01-13 18:55:47 +0530

    changes

--- PSR copy/algo4/bash.sh
+++
@@ -1,8 +0,0 @@
-cd "/root/development/AkashLiveAlgosBacktests/PSR/algo4/PSR_123_BN"
-/usr/local/bin/pm2 start "PSR_123_BN_without_DSL.py" --interpreter="/root/development/AkashLiveAlgosBacktests/venv/bin/python3" --name="PSR_123_BN_without_DSL-1" --no-autorestart --time
-
-cd "/root/development/AkashLiveAlgosBacktests/PSR/algo4/PSR_123_N"
-/usr/local/bin/pm2 start "PSR_123_N_without_DSL.py" --interpreter="/root/development/AkashLiveAlgosBacktests/venv/bin/python3" --name="PSR_123_N_without_DSL-1" --no-autorestart --time
-
-cd "/root/development/AkashLiveAlgosBacktests/PSR/algo4/PSR_123_SS"
-/usr/local/bin/pm2 start "PSR_123_SS_without_DSL.py" --interpreter="/root/development/AkashLiveAlgosBacktests/venv/bin/python3" --name="PSR_123_SS_without_DSL-1" --no-autorestart --time
--- PSR copy/algo5/bash.sh
+++
@@ -1,8 +0,0 @@
-cd "/root/AkashLiveAlgosBacktests/PSR/algo5/PSR_3PM_BN"
-/usr/local/bin/pm2 start "PSR_3PM_BN.py" --interpreter="/root/AkashLiveAlgosBacktests/venv/bin/python3" --name="PSR_3PM_BN-1" --no-autorestart --time
-
-cd "/root/AkashLiveAlgosBacktests/PSR/algo5/PSR_3PM_N"
-/usr/local/bin/pm2 start "PSR_3PM_N.py" --interpreter="/root/AkashLiveAlgosBacktests/venv/bin/python3" --name="PSR_3PM_N-1" --no-autorestart --time
-
-cd "/root/AkashLiveAlgosBacktests/PSR/algo5/PSR_3PM_SS"
-/usr/local/bin/pm2 start "PSR_3PM_SS.py" --interpreter="/root/AkashLiveAlgosBacktests/venv/bin/python3" --name="PSR_3PM_SS-1" --no-autorestart --time
